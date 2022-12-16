094b3b93323eea1b760d788263086ae531ec1b67
Wangsh-leo <103399011+Wangsh-leo@users.noreply.github.com>
2022-12-16 14:32:26 +0800

    chore: add todolist

---
+++ src/components/Todolist.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+let count = ref(0)
+const add = function(){
+    count.value++
+}
+</script>
+<template>
+    <h1 @click="add">{{ count }}</h1>
+</template>
+
+<style>
+h1 {
+    color: red;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server:{
+    open:'http://127.0.0.1:5173/',
+    port: 5173,
+  }
 })
